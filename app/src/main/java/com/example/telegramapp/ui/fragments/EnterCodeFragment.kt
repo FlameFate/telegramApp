4f8322f66f1ec5d1affa4fb19bebb7c3ec37c19d
FlameFate <egor.androsoff@gmail.com>
2022-07-23 22:24:39 +0300

    Code Fragment

---
+++ app/src/main/java/com/example/telegramapp/ui/fragments/EnterCodeFragment.kt
@@ -0,0 +1,10 @@
+package com.example.telegramapp.ui.fragments
+
+
+import androidx.fragment.app.Fragment
+import com.example.telegramapp.R
+
+
+class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
+
+}
---
+++ app/src/main/java/com/example/telegramapp/ui/fragments/EnterPhoneNumberFragment.kt
@@ -0,0 +1,9 @@
+package com.example.telegramapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.telegramapp.R
+
+
+class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) {
+
+}
